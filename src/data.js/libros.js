934c33800fb39e18ca06d5a7da4d2c0cac3eaeec
Stefano <stragastefano@gmail.com>
2023-10-06 00:20:24 +0200

    my first commit

---
+++ src/data.js/libros.js
@@ -0,0 +1,73 @@
+// Datos de ejemplo 
+export const libros = [
+    {
+        id: 1,
+        titulo: "1984",
+        autor: "George Orwell",
+        categoria: "Distopia",
+        descripcion: "Un clasico sobre la vigilancia gubernamental y la opresion totalitaria"
+    },
+    {
+        id: 2,
+        titulo: "Marginal",
+        autor: "Mario Borges",
+        categoria: "Aventura",
+        descripcion: "Un clasico sobre la vigilancia gubernamental y la opresion totalitaria"
+    },
+    {
+        id: 3,
+        titulo: "El principito",
+        autor: "Rick",
+        categoria: "Horror",
+        descripcion: "Un clasico sobre la vigilancia gubernamental y la opresion totalitaria"
+    },
+    {
+        id: 4,
+        titulo: "El señor de los anillos",
+        autor: "Ana Frank",
+        categoria: "Ficcion",
+        descripcion: "Un clasico sobre la vigilancia gubernamental y la opresion totalitaria"
+    },
+    {
+        id: 5,
+        titulo: "El gran fiorino",
+        autor: "Anna",
+        categoria: "Distopia",
+        descripcion: "Un clasico sobre la vigilancia gubernamental y la opresion totalitaria"
+    },
+    {
+        id: 6,
+        titulo: "Sutopia",
+        autor: "F. Scott",
+        categoria: "Clasico",
+        descripcion: "Un clasico sobre la vigilancia gubernamental y la opresion totalitaria"
+    },
+    {
+        id: 7,
+        titulo: "Live",
+        autor: "George Orwell",
+        categoria: "Distopia",
+        descripcion: "Un clasico sobre la vigilancia gubernamental y la opresion totalitaria"
+    },
+    {
+        id: 8,
+        titulo: "The best",
+        autor: "Gragas",
+        categoria: "Clasico",
+        descripcion: "Un clasico sobre la vigilancia gubernamental y la opresion totalitaria"
+    },
+    {
+        id: 9,
+        titulo: "The happy book",
+        autor: "Mark Antonny",
+        categoria: "Alegria",
+        descripcion: "Un clasico sobre la vigilancia gubernamental y la opresion totalitaria"
+    },
+    {
+        id: 10,
+        titulo: "Greenday",
+        autor: "Marcos Donne't",
+        categoria: "Alegria",
+        descripcion: "Un clasico sobre la vigilancia gubernamental y la opresion totalitaria"
+    }
+]
